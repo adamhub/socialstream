2fc5ccd22fe4a2f5da4ec4a854fbb595c94bbae6
adamhub <adam@adamtemple.co>
2021-07-13 19:20:48 -0700

    update nave title and links

--- foss/settings.py
+++ foss/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 ROOT_URLCONF = 'foss.urls'
